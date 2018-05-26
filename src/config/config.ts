57cea648d6dffd66e70fa3f53bd83fd306899a3b
ninjadev1030 <alexandra.uefa@gmail.com>
2018-05-26 13:49:13 +0800

    add config file

---
+++ src/config/config.ts
@@ -0,0 +1,25 @@
+export let CONFIG = {
+    /*** Stage **/
+    SEARCH_LIVE_EVENTS: {
+        url:"dori-us-east-1.searchly.com",
+        index:"live-events",
+        key:"kzv8ntali8iffhfvcxhk1noqhwfigccp",
+        access_key_name:"read_only"
+    },
+
+    /*** Local ***
+     SEARCH_LIVE_EVENTS: {
+        url:"nori-us-east-1.searchly.com",
+        index:"live-events",
+        key:"a93t8ypobflr6fws6edhktx59oqlruyh",
+        access_key_name:"search-live-events"
+    },
+
+    PRO_PAY:{
+        baseURI:"https://protectpaytest.propay.com",
+        hppURI:"/hpp/home/",
+        signalrURI:"/hpp/signalr/"
+    }
+    **/
+
+}
