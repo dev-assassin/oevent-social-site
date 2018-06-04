bab56e698a91c8391ff7e0bcc71b826a6a1a0ca2
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-05 06:22:05 +0800

    add forgot-password component

---
+++ src/app/app-components/forgot-password.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from "../auth/services/auth-service";
+import { NgbActiveModal } from "@ng-bootstrap/ng-bootstrap";
+import { AppService } from "../services/app-service";
+import {ToastyService} from "ng2-toasty";
+import {AccountContactService} from "../shared-module/services/account-contact-service";
+import {AccountMeService} from "../shared-module/services/account-me-service";
+import {IAccountContact} from "../account/components/contact/contact-model";
+import { FirebaseApp, AngularFireModule } from 'angularfire2';
+
+@Component({
+    selector: 'fl-forgot-password',
+    styles: [
+       `
+            .login-error{padding-bottom:15px;}
+        `
+    ],
+    templateUrl: './forgot-password.component.html'
+})
+
+export class ForgotPasswordComponent {
+    email:string;
+    emailSent:boolean = false;
+
+    constructor(
+        public appService: AppService,
+        public auth: AuthService,
+        private router: Router,
+        public activeModal: NgbActiveModal,
+        public toasty: ToastyService,
+        private contactService: AccountContactService,
+        private aboutService: AccountMeService,
+        private app: FirebaseApp
+        ) {
+    }
+
+    login(){
+        this.appService.openLogin();
+    }
+
+    onSubmit(form){
+
+        if(form.valid){
+            let email = form.controls['email'].value;
+
+            this.app.auth().sendPasswordResetEmail(email).then(()=>{
+                this.emailSent = true;
+            }, ()=>{
+
+            });
+
+        }
+
+    }
+
+}
