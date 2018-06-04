43eb9fffbffe7004ec7012e278057d4f8db530a9
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-04 17:12:45 +0800

    add account component

---
+++ src/app/account/components/account.ts
@@ -0,0 +1,19 @@
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/pluck';
+
+import {Component, OnDestroy} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AccountService } from '../services/account-service';
+
+@Component({
+    templateUrl: './account.html'
+})
+
+export class AccountComponent implements OnDestroy{
+    constructor(public route: ActivatedRoute, public accountService: AccountService) {
+    }
+
+    ngOnDestroy(){
+        this.accountService.onDestroy();
+    }
+}
