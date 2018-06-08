18c1ddcd9134468d32aafa444a68c47e466cee00
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-08 22:51:42 +0800

    add edit/modal

---
+++ src/app/create/components/edit/modal/event-map.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from "@angular/core";
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+import {CreateEventService} from "../../../services/create-event.service";
+import {IGPS} from "../../../../shared-models/gps";
+
+@Component({
+               selector: 'event-map',
+               styles: [
+                   `
+                    agm-map {
+                         height: 300px;
+                         width:350px;
+                    }
+                   `
+               ],
+               template: `
+    <div ngbModalContainer>
+        <div class="modal-body" >
+            <div>
+                <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss()" style="margin:15px 3px;"><span aria-hidden="true">×</span></button>
+            </div>
+            <div>
+                <agm-map [latitude]="gps.lat" [longitude]="gps.lng">
+                  <agm-marker [latitude]="gps.lat" [longitude]="gps.lng"></agm-marker>
+                </agm-map>
+            </div>
+        </div>
+    </div>
+`
+           })
+
+export class EventMapComponent implements OnInit
+{
+    gps: IGPS;
+
+    constructor(public activeModal: NgbActiveModal,
+                private createService: CreateEventService)
+    {
+    }
+
+    ngOnInit(): void
+    {
+      this.gps = this.createService.draft.gps;
+    }
+
+}
