b27ea1f9bf65341874684cb7cabb0d2a6fafcc88
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-12 17:46:11 +0800

    update title component

--- src/app/create/components/edit/_title.component.ts
+++ src/app/create/components/edit/_title.component.ts
@@ -1,71 +1,66 @@
-import {Component, AfterContentInit, OnInit, Input} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params} from '@angular/router';
-import {AppService} from "../../../services/app-service";
-import {AuthService} from "../../../auth/services/auth-service";
-import {CreateEventService} from "../../services/create-event.service";
-import {FieldValidation} from "../../../../assets/common/validation/field-validation";
+import { Component, AfterContentInit, OnInit, Input } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params } from '@angular/router';
+import { AppService } from '../../../services/app-service';
+import { AuthService } from '../../../auth/services/auth-service';
+import { CreateEventService } from '../../services/create-event.service';
+import { FieldValidation } from '../../../../assets/common/validation/field-validation';
 
 @Component({
-               selector: 'event-title',
+    selector: 'app-event-title',
 
-               template: `
+    template: `
         <div class="{{!createService.isFieldValid('title')?'has-error':''}}">
         <label>
             Event Name
         </label>
         <input type="text" id="title" class="form-control" [(ngModel)]="title" (blur)="saveTitle()"/>
-        <div *ngIf="!createService.isFieldValid('title')" class="validation-error-text">{{createService.fieldValidations.get("title").errorMessage}}</div> 
+        <div *ngIf="!createService.isFieldValid('title')" class="validation-error-text">
+            {{createService.fieldValidations.get("title").errorMessage}}
+        </div>
         </div>
     `,
-               styles: [
-                   `
+    styles: [
+        `
             :host{
                 width:100%;
-            }  
+            }
         `
-               ],
+    ],
 
-           })
+})
 
-export class EditTitleComponent implements OnInit
-{
+export class EditTitleComponent implements OnInit {
 
     title: string;
 
     constructor(private auth: AuthService,
-                private appService: AppService,
-                private router: Router,
-                private route: ActivatedRoute,
-                private createService: CreateEventService)
-    {
+        private appService: AppService,
+        private router: Router,
+        private route: ActivatedRoute,
+        private createService: CreateEventService) {
 
     }
 
-    ngOnInit()
-    {
+    ngOnInit() {
         this.title = this.createService.draft.title;
     }
 
-    saveTitle()
-    {
-        this.createService.initValidationField("title");
-        if (this.title && this.title.trim() != "") {
-            this.createService.draftObject$.update({"title": this.title, "status":"draft"})
-                .then(()=>
-                      {
-                          this.createService.showSavedDraft();
-                      });
+    saveTitle() {
+        this.createService.initValidationField('title');
+        if (this.title && this.title.trim() !== '') {
+            this.createService.draftObject$.update({ 'title': this.title, 'status': 'draft' })
+                .then(() => {
+                    this.createService.showSavedDraft();
+                });
 
             if (this.createService.isLive()) {
                 this.createService.publish()
             }
 
-        }
-        else {
-            this.createService.setErrorInField("title","Event name is a required field");
+        } else {
+            this.createService.setErrorInField('title', 'Event name is a required field');
         }
 
     }
-
 
 }
