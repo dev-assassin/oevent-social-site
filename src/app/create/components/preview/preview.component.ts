84151920f35bfbcaa7a550636508081e393126a6
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-13 10:33:22 +0800

    add preview component

---
+++ src/app/create/components/preview/preview.component.ts
@@ -0,0 +1,35 @@
+import {Component, AfterContentInit, OnInit} from '@angular/core';
+import {Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params} from '@angular/router';
+import {AppService} from "../../../services/app-service";
+import {AuthService} from "../../../auth/services/auth-service";
+import {PreviewService} from "../../services/preview.service";
+
+@Component({
+    templateUrl: './preview.component.html',
+    styles: [
+        `
+           
+        `
+    ],
+
+})
+
+export class PreviewComponent implements OnInit{
+
+    idSet:boolean = true;
+    eventId:string;
+
+    constructor(private auth: AuthService,
+                private appService: AppService,
+                private router: Router,
+                private route: ActivatedRoute, private previewService: PreviewService) {
+
+    }
+
+    ngOnInit(){
+        this.eventId = this.previewService.eventId;
+        this.idSet = true;
+    }
+
+
+}
