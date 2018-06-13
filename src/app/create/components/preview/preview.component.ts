ebc8e67b9cc7a63213416eb0234b3c65486682ca
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-13 11:12:59 +0800

    update preview component

--- src/app/create/components/preview/preview.component.ts
+++ src/app/create/components/preview/preview.component.ts
@@ -1,32 +1,30 @@
-import {Component, AfterContentInit, OnInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params} from '@angular/router';
-import {AppService} from "../../../services/app-service";
-import {AuthService} from "../../../auth/services/auth-service";
-import {PreviewService} from "../../services/preview.service";
+import { Component, AfterContentInit, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params } from '@angular/router';
+import { AppService } from '../../../services/app-service';
+import { AuthService } from '../../../auth/services/auth-service';
+import { PreviewService } from '../../services/preview.service';
 
 @Component({
     templateUrl: './preview.component.html',
     styles: [
-        `
-           
-        `
+        ``
     ],
 
 })
 
-export class PreviewComponent implements OnInit{
+export class PreviewComponent implements OnInit {
 
-    idSet:boolean = true;
-    eventId:string;
+    idSet: Boolean = true;
+    eventId: string;
 
     constructor(private auth: AuthService,
-                private appService: AppService,
-                private router: Router,
-                private route: ActivatedRoute, private previewService: PreviewService) {
+        private appService: AppService,
+        private router: Router,
+        private route: ActivatedRoute, private previewService: PreviewService) {
 
     }
 
-    ngOnInit(){
+    ngOnInit() {
         this.eventId = this.previewService.eventId;
         this.idSet = true;
     }
