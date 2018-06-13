d774e21163d65a5f9117b6818e0cc1883a20a854
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-13 09:34:22 +0800

    update edit component

--- src/app/create/components/edit/edit.component.ts
+++ src/app/create/components/edit/edit.component.ts
@@ -1,34 +1,32 @@
-import {Component, AfterContentInit, OnInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params} from '@angular/router';
-import {AppService} from "../../../services/app-service";
-import {AuthService} from "../../../auth/services/auth-service";
-import {CreateEventService} from "../../services/create-event.service";
-import {CreateEventDateService} from "../../services/create-date.service";
+import { Component, AfterContentInit, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params } from '@angular/router';
+import { AppService } from '../../../services/app-service';
+import { AuthService } from '../../../auth/services/auth-service';
+import { CreateEventService } from '../../services/create-event.service';
+import { CreateEventDateService } from '../../services/create-date.service';
 
 @Component({
     templateUrl: './edit.component.html',
     styles: [
-        `
-           
-        `
+        ``
     ],
 
 })
 
-export class EditComponent implements OnInit{
+export class EditComponent implements OnInit {
 
-    date:string;
+    date: string;
 
     constructor(private auth: AuthService,
-                private appService: AppService,
-                private router: Router,
-                private route: ActivatedRoute,
-                private createService: CreateEventService,
-                private dateService: CreateEventDateService) {
+        private appService: AppService,
+        private router: Router,
+        private route: ActivatedRoute,
+        private createService: CreateEventService,
+        private dateService: CreateEventDateService) {
 
     }
 
-    ngOnInit(){
+    ngOnInit() {
         this.createService.setEventId(this.createService.eventId);
     }
 
