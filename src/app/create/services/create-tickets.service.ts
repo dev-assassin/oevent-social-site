cc8360bd7b288618b42e15411c0ddd41562392e2
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-14 10:06:48 +0800

    update create-tickets service

--- src/app/create/services/create-tickets.service.ts
+++ src/app/create/services/create-tickets.service.ts
@@ -1,40 +1,40 @@
-import {Injectable, OnDestroy} from '@angular/core';
-import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
-import {ITicket, Ticket} from "../models/tickets.models";
-import {CreateEventService} from "./create-event.service";
-import {AuthService} from "../../auth/services/auth-service";
+import { Injectable, OnDestroy } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { ITicket, Ticket } from '../models/tickets.models';
+import { CreateEventService } from './create-event.service';
+import { AuthService } from '../../auth/services/auth-service';
 
 @Injectable()
-export class CreateTicketsService{
+export class CreateTicketsService {
 
-    ticketList$:FirebaseListObservable<ITicket[]>;
-    ticketListObject$:FirebaseObjectObservable<any>;
-    eventId:string;
+    ticketList$: FirebaseListObservable<ITicket[]>;
+    ticketListObject$: FirebaseObjectObservable<any>;
+    eventId: string;
 
     constructor(private af: AngularFireDatabase,
-                private auth: AuthService
-                ) {
+        private auth: AuthService
+    ) {
 
     }
 
-    getTicketObject(key):FirebaseObjectObservable<ITicket>{
-        let ticketObject$:FirebaseObjectObservable<ITicket> = this.af.object(`drafts/tickets/${this.eventId}/${key}`);
+    getTicketObject(key): FirebaseObjectObservable<ITicket> {
+        const ticketObject$: FirebaseObjectObservable<ITicket> = this.af.object(`drafts/tickets/${this.eventId}/${key}`);
         return ticketObject$;
     }
 
-    initTickets(eventId){
+    initTickets(eventId) {
         this.ticketList$ = this.af.list(`drafts/tickets/${eventId}`);
         this.eventId = eventId;
-        this.ticketListObject$ = this.af.object(`drafts/tickets/${eventId}`, {preserveSnapshot: true});
+        this.ticketListObject$ = this.af.object(`drafts/tickets/${eventId}`, { preserveSnapshot: true });
     }
 
-    addTicket(ticketType){
-        let ticket:ITicket = new Ticket(this.eventId);
+    addTicket(ticketType) {
+        const ticket: ITicket = new Ticket(this.eventId);
         ticket.ticketType = ticketType;
         this.ticketList$.push(ticket);
     }
 
-    removeTicket(key){
+    removeTicket(key) {
         console.log('remove in service');
         this.ticketList$.remove(key);
     }
