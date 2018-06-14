219e92d2ac0f58dad34217f2c0c3b1dcbfd8429c
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-15 00:08:40 +0800

    update email-action component

--- src/app/email-action/components/email-action.component.ts
+++ src/app/email-action/components/email-action.component.ts
@@ -1,10 +1,10 @@
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/pluck';
 
-import {Component, AfterContentInit, OnInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params} from '@angular/router';
-import {AuthService} from "../../auth/services/auth-service";
-import {AppService} from "../../services/app-service";
+import { Component, AfterContentInit, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params } from '@angular/router';
+import { AuthService } from '../../auth/services/auth-service';
+import { AppService } from '../../services/app-service';
 
 
 @Component({
@@ -13,28 +13,28 @@ import {AppService} from "../../services/app-service";
         `
            a.selected{
                 color:#014c8c;
-           } 
+           }
         `
     ],
 
 })
 
-export class EmailActionComponent implements OnInit{
+export class EmailActionComponent implements OnInit {
 
     view = '';
 
     constructor(private auth: AuthService,
-                private appService: AppService,
-                private router: Router,
-                private route: ActivatedRoute) {
+        private appService: AppService,
+        private router: Router,
+        private route: ActivatedRoute) {
 
     }
 
-    ngOnInit(){
-        this.route.queryParams.subscribe((params:Params)=>{
+    ngOnInit() {
+        this.route.queryParams.subscribe((params: Params) => {
             this.view = params['mode'];
             console.log(params);
-        })
+        });
     }
 
 
