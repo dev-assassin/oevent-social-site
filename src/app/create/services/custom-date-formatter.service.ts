34de4ebdeb5a1fb25e44febb04834f26cafc0963
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-14 11:15:27 +0800

    update custom-date-formatter service

--- src/app/create/services/custom-date-formatter.service.ts
+++ src/app/create/services/custom-date-formatter.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from "@angular/core";
-import { NgbDateParserFormatter, NgbDateStruct } from "@ng-bootstrap/ng-bootstrap";
+import { Injectable } from '@angular/core';
+import { NgbDateParserFormatter, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 function padNumber(value: number) {
     if (isNumber(value)) {
         return `0${value}`.slice(-2);
     } else {
-        return "";
+        return '';
     }
 }
 
@@ -24,21 +24,21 @@ export class NgbDateFRParserFormatter extends NgbDateParserFormatter {
         if (value) {
             const dateParts = value.trim().split('/');
             if (dateParts.length === 1 && isNumber(dateParts[0])) {
-                return {year: toInteger(dateParts[0]), month: null, day: null};
+                return { year: toInteger(dateParts[0]), month: null, day: null };
             } else if (dateParts.length === 2 && isNumber(dateParts[0]) && isNumber(dateParts[1])) {
-                return {year: toInteger(dateParts[1]), month: toInteger(dateParts[0]), day: null};
+                return { year: toInteger(dateParts[1]), month: toInteger(dateParts[0]), day: null };
             } else if (dateParts.length === 3 && isNumber(dateParts[0]) && isNumber(dateParts[1]) && isNumber(dateParts[2])) {
-                return {year: toInteger(dateParts[2]), month: toInteger(dateParts[1]), day: toInteger(dateParts[0])};
+                return { year: toInteger(dateParts[2]), month: toInteger(dateParts[1]), day: toInteger(dateParts[0]) };
             }
         }
         return null;
     }
 
     format(date: NgbDateStruct): string {
-        let stringDate: string = "";
-        if(date) {
-            stringDate += isNumber(date.day) ? padNumber(date.day) + "/" : "";
-            stringDate += isNumber(date.month) ? padNumber(date.month) + "/" : "";
+        let stringDate = '';
+        if (date) {
+            stringDate += isNumber(date.day) ? padNumber(date.day) + '/' : '';
+            stringDate += isNumber(date.month) ? padNumber(date.month) + '/' : '';
             stringDate += date.year;
         }
         return stringDate;
