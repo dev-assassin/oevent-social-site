f46a281c3f741c19870dc978676d71505c8b6c2d
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-18 20:11:10 +0800

    add registrant.details.component.ts file

---
+++ src/app/event/components/my-event/_registrant.detail.component.ts
@@ -0,0 +1,90 @@
+import {Component, Input, OnInit, AfterViewInit, AfterViewChecked} from '@angular/core';
+import {ToastyService} from "ng2-toasty";
+import {AngularFireDatabase} from "angularfire2/database"
+import {IAccountAbout} from "../../../account/components/about/about-model";
+import {Router} from "@angular/router";
+declare var Clipboard:any;
+
+@Component({
+    selector: 'o-registrant',
+    styles: [
+        `
+            
+            
+        `
+    ],
+    template: `
+        
+        <div class="row c-cart-table-row margin-vertical-sm" *ngIf="profileSet">
+            <div class="col-md-2 col-sm-3 col-sm-5 c-cart-image">
+                <a (click)="gotoProfile()" target="blank" *ngIf="ocodeSet">
+                    <img [src]="profile.imageURL" alt="" class="img-fluid">
+                </a>    
+            </div>
+            <div class="col-md-3 col-sm-3 col-sm-6 c-cart-ref">
+                
+                <a (click)="gotoProfile()" >
+                    <p>{{ profile.organizerName }}</p>
+                </a>
+            </div>
+            <div class="col-md-5 col-sm-6 col-sm-6 c-cart-desc">
+                
+                <p>
+                    <a class="c-font-bold c-theme-link c-font-dark">{{ children.length }}</a>
+                </p>
+            </div>
+        </div>
+        
+        
+        
+        
+    `
+})
+
+export class RegistrantComponent implements OnInit, AfterViewChecked{
+
+    @Input() profileId;
+    @Input() children;
+    profile:IAccountAbout;
+    profileSet:boolean = false;
+    ocodeSet:boolean = false;
+    ocode:string = "";
+
+    constructor(private af: AngularFireDatabase, private router: Router) {
+
+    }
+
+    ngOnInit(){
+        this.populateProfile();
+        this.getOcode();
+    }
+
+    ngAfterViewChecked(){
+
+    }
+
+    gotoProfile(){
+        this.router.navigateByUrl(`/${this.ocode}`);
+    }
+
+    getOcode(){
+        this.af.object(`/ocodesById/${this.profileId}`).first().subscribe((data)=>{
+            this.ocode = data.ocode;
+            this.ocodeSet=true;
+        });
+    }
+
+    populateProfile(){
+
+        return this.af.object(`/about/${this.profileId}/`).first().subscribe((profile)=>{
+            this.profile = profile;
+            this.profileSet = true;
+        });
+
+    }
+
+
+
+
+
+}
