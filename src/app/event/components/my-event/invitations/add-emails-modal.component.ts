8bddf424a15d2b4b5887b855777c76e6043336c5
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-18 08:27:54 +0800

    update add-emails-modal.component.ts file

--- src/app/event/components/my-event/invitations/add-emails-modal.component.ts
+++ src/app/event/components/my-event/invitations/add-emails-modal.component.ts
@@ -1,35 +1,32 @@
-import {Component, OnInit} from "@angular/core";
-import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
-import {EventService} from "../../../services/event-service";
-import {InviteEmail} from "../../../models/invite-email";
-import {FirebaseListObservable} from "angularfire2/database/firebase_list_observable";
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { EventService } from '../../../services/event-service';
+import { InviteEmail } from '../../../models/invite-email';
+import { FirebaseListObservable } from 'angularfire2/database/firebase_list_observable';
 import * as Papa from 'papaparse/papaparse.min.js';
 
 @Component({
-               selector: 'invitation-emails',
-               styles: [
-               ],
-               templateUrl: './add-emails-modal.html'
-           })
+    selector: 'app-invitation-emails',
+    styles: [
+    ],
+    templateUrl: './add-emails-modal.html'
+})
 
-export class AddEmailsModalComponent implements OnInit
-{
+export class AddEmailsModalComponent implements OnInit {
 
-    addOption:string;
-    emails:string;
-    inviteEmails$:FirebaseListObservable<any>;
-    inviteEmails:InviteEmail[];
+    addOption: string;
+    emails: string;
+    inviteEmails$: FirebaseListObservable<any>;
+    inviteEmails: InviteEmail[];
 
     constructor(public activeModal: NgbActiveModal,
-                public eventService: EventService
-    )
-    {
-        this.addOption = "manual";
-        if(eventService.set){
+        public eventService: EventService
+    ) {
+        this.addOption = 'manual';
+        if (eventService.set) {
             this.setInvite();
-        }
-        else{
-            this.eventService.eventUpdated.first().subscribe(()=>{
+        } else {
+            this.eventService.eventUpdated.first().subscribe(() => {
                 this.setInvite();
             });
         }
@@ -37,62 +34,59 @@ export class AddEmailsModalComponent implements OnInit
 
     }
 
-    ngOnInit(): void
-    {
+    ngOnInit(): void {
 
     }
 
-    setInvite(){
-        if(this.eventService.inviteKey != "promoter"){
+    setInvite() {
+        if (this.eventService.inviteKey !== 'promoter') {
             this.inviteEmails$ = this.eventService.getInviteEmails();
-        }
-        else{
+        } else {
             this.inviteEmails$ = this.eventService.getPromoterInviteEmails();
         }
     }
 
-    addEmails(){
-        if(this.addOption == "manual") {
-            let lines = this.emails.split("\n");
-            let uniqueEmails = new Set<string>();
-            for (let line of lines) {
-                let lineEmails = line.split(",");
-                for (let email of lineEmails) {
+    addEmails() {
+        if (this.addOption === 'manual') {
+            const lines = this.emails.split('\n');
+            const uniqueEmails = new Set<string>();
+            for (const line of lines) {
+                const lineEmails = line.split(',');
+                for (const email of lineEmails) {
                     uniqueEmails.add(email.trim().toLowerCase());
+                }
             }
-            }
-            uniqueEmails.forEach((email) =>
-                                 {
-                                     let inviteEmail: InviteEmail = new InviteEmail();
-                                     inviteEmail.email = email;
-                                     this.inviteEmails$.push(inviteEmail);
-                                 });
-        }else if(this.addOption == "from_file"){
-            for(let inviteEmail of this.inviteEmails){
+            uniqueEmails.forEach((email) => {
+                const inviteEmail: InviteEmail = new InviteEmail();
+                inviteEmail.email = email;
+                this.inviteEmails$.push(inviteEmail);
+            });
+        } else if (this.addOption === 'from_file') {
+            for (const inviteEmail of this.inviteEmails) {
                 this.inviteEmails$.push(inviteEmail);
             }
         }
         this.activeModal.dismiss();
     }
 
-    fileChangeListener($event):void {
+    fileChangeListener($event): void {
         this.uploadAction($event.target);
     }
 
-    uploadAction(inputValue:any):void {
-        var file:File = inputValue.files[0];
-        let localArr = [];
+    uploadAction(inputValue: any): void {
+        const file: File = inputValue.files[0];
+        const localArr = [];
         Papa.parse(file, {
-            complete: function(results) {
+            complete: function (results) {
                 let firstRow = true;
-                for(var row of results.data){
-                    if(!firstRow) {
-                        let inviteEmail: InviteEmail = new InviteEmail();
+                for (const row of results.data) {
+                    if (!firstRow) {
+                        const inviteEmail: InviteEmail = new InviteEmail();
                         inviteEmail.email = row[0];
                         inviteEmail.firstName = row[1];
                         inviteEmail.lastName = row[2];
                         localArr.push(inviteEmail);
-                    }else{
+                    } else {
                         firstRow = false;
                     }
 
