d1077d4a5c4df619c8c8db67a50072ad3c93c42a
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-21 06:56:35 +0800

    add sent-message component.ts file

---
+++ src/app/event/components/my-event/sent-messages.component.ts
@@ -0,0 +1,38 @@
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/pluck';
+
+import { Component, AfterContentInit, OnInit, Input } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth-service';
+import { EventService } from '../../services/event-service';
+import { IoEvent, oEvent } from '../../../shared-models/oevent';
+import { PromoteModalComponent } from '../event/modals/promote-modal-component';
+import { FirebaseListObservable, AngularFireDatabase } from 'angularfire2/database'
+import { AttendModalComponent } from '../event/modals/attend-modal-component';
+declare var Quill: any;
+
+@Component({
+    selector: 'app-my-event-sent-emails',
+    template: `
+        <h3 class="line" style="margin-bottom:0px;">
+            {{ eventService.event.title }} - Sent Emails
+        </h3>
+    `,
+    styles: [
+        `
+
+        `
+    ]
+
+})
+
+export class MyEventSentEmailsComponent implements OnInit {
+
+    constructor(private af: AngularFireDatabase, public eventService: EventService) {
+
+    }
+
+    ngOnInit() {
+
+    }
+}
