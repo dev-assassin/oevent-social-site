73559da9cd49ba7497855d7bb91f82ab6a1de906
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-21 10:05:16 +0800

    update account-contact-component.ts file

--- src/app/account/components/contact/account-contact-component.ts
+++ src/app/account/components/contact/account-contact-component.ts
@@ -1,62 +1,61 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {AuthService} from "../../../auth/services/auth-service";
-import {AccountContactService} from "../../../shared-module/services/account-contact-service";
-import {IAccountContact, AccountContact} from "./contact-model";
-import {ToastyService} from "ng2-toasty/index";
-import {AppService} from "../../../services/app-service";
-import {AccountService} from "../../services/account-service";
-import {FirebaseObjectObservable} from "angularfire2/database/firebase_object_observable";
-import {AngularFireDatabase} from "angularfire2/database/database";
+import { AuthService } from '../../../auth/services/auth-service';
+import { AccountContactService } from '../../../shared-module/services/account-contact-service';
+import { IAccountContact, AccountContact } from './contact-model';
+import { ToastyService } from 'ng2-toasty/index';
+import { AppService } from '../../../services/app-service';
+import { AccountService } from '../../services/account-service';
+import { FirebaseObjectObservable } from 'angularfire2/database/firebase_object_observable';
+import { AngularFireDatabase } from 'angularfire2/database/database';
 
 @Component({
-    selector: 'account-contact',
+    selector: 'app-account-contact',
     styles: [],
     templateUrl: './contact.html'
 })
 
 export class AccountContactComponent {
     values: IAccountContact = new AccountContact();
-    contact$:FirebaseObjectObservable<IAccountContact> = new FirebaseObjectObservable();
+    contact$: FirebaseObjectObservable<IAccountContact> = new FirebaseObjectObservable();
 
     constructor(public auth: AuthService,
-                private af: AngularFireDatabase,
-                private accountService: AccountService,
-                private toasty: ToastyService,
-                private appService: AppService){
+        private af: AngularFireDatabase,
+        private accountService: AccountService,
+        private toasty: ToastyService,
+        private appService: AppService) {
 
         this.appService.startLoadingBar();
 
-        if(this.auth.authenticated){
+        if (this.auth.authenticated) {
             this.populate();
-        }
-        else{
-            this.auth.authObservable.first().subscribe(()=>{
+        } else {
+            this.auth.authObservable.first().subscribe(() => {
                 this.populate();
-            })
+            });
         }
 
     }
 
-    populate(){
+    populate() {
 
         this.contact$ = this.af.object(`/contact/${this.auth.id}`);
-        let contact:any;
-        contact = this.contact$.subscribe((values)=>{
+        let contact: any;
+        contact = this.contact$.subscribe((values) => {
             this.values = values;
         });
-        this.accountService.destroyObservable.subscribe((data)=>{if(data){contact.unsubscribe()}});
+        this.accountService.destroyObservable.subscribe((data) => { if (data) { contact.unsubscribe(); } });
         this.appService.stopLoadingBar();
     }
 
-    onSubmit(form:any){
+    onSubmit(form: any) {
 
-        if(form.valid){
-            let cleanedValues = {};
-            let values = form.value;
+        if (form.valid) {
+            const cleanedValues = {};
+            const values = form.value;
 
-            for(let key in values){
-                if(typeof values[key] != "undefined"){
+            for (const key in values) {
+                if (typeof values[key] !== 'undefined') {
                     cleanedValues[key] = values[key];
                 }
             }
@@ -65,13 +64,13 @@ export class AccountContactComponent {
 
                 this.appService.startLoadingBar();
 
-                this.contact$.set(cleanedValues).then(()=>{
+                this.contact$.set(cleanedValues).then(() => {
                     this.appService.completeLoadingBar();
-                    this.toasty.success("Your contact information has been saved!");
-                }, (err)=>{
+                    this.toasty.success('Your contact information has been saved!');
+                }, (err) => {
 
-                    let toast = {
-                        title: "Failed",
+                    const toast = {
+                        title: 'Failed',
                         msg: `Your contact information was not saved: ${err.message}`
                     };
                     this.appService.completeLoadingBar();
@@ -80,9 +79,10 @@ export class AccountContactComponent {
                 });
 
             }
-            catch(err){
-                let toast = {
-                    title: "Failed",
+            // tslint:disable-next-line:one-line
+            catch (err) {
+                const toast = {
+                    title: 'Failed',
                     msg: `Your contact information was not saved: ${err}`
                 };
                 this.appService.completeLoadingBar();
