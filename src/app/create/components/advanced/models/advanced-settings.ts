f33805653deef90c526d239bae3b9eabd535f1c7
ninjadev1030 <alexandra.uefa@gmail.com>
2018-06-22 15:29:57 +0800

    update model and services in advanced component

--- src/app/create/components/advanced/models/advanced-settings.ts
+++ src/app/create/components/advanced/models/advanced-settings.ts
@@ -1,112 +1,114 @@
-import {QuillDelta} from "../../../../models/IQuillDelta";
+import { QuillDelta } from '../../../../models/IQuillDelta';
 
-export class EventSettings{
-    PromoterWelcome:PromoterWelcomeEmailSettings = new PromoterWelcomeEmailSettings();
-    RegistrantWelcome:RegistrantWelcomeEmailSettings = new RegistrantWelcomeEmailSettings();
-    EventReminder:EventReminderEmailSettings = new EventReminderEmailSettings();
-    EventCompletion:EventCompletionEmailSettings = new EventCompletionEmailSettings();
-    RegistrationFields:EventFormFields = new EventFormFields();
-    Confirmation:EventConfirmationSettings = new EventConfirmationSettings();
+export class EventSettings {
+    PromoterWelcome: PromoterWelcomeEmailSettings = new PromoterWelcomeEmailSettings();
+    RegistrantWelcome: RegistrantWelcomeEmailSettings = new RegistrantWelcomeEmailSettings();
+    EventReminder: EventReminderEmailSettings = new EventReminderEmailSettings();
+    EventCompletion: EventCompletionEmailSettings = new EventCompletionEmailSettings();
+    RegistrationFields: EventFormFields = new EventFormFields();
+    Confirmation: EventConfirmationSettings = new EventConfirmationSettings();
 }
 
-export interface IEmailSettings{
-    enabled?:boolean;
-    daysBefore?:number;
-    daysAfter?:number;
-    subject:string;
-    body:any;
-    html:string;
+export interface IEmailSettings {
+    enabled?: boolean;
+    daysBefore?: number;
+    daysAfter?: number;
+    subject: string;
+    body: any;
+    html: string;
 }
 
-export class PromoterWelcomeEmailSettings implements IEmailSettings{
-    enabled:boolean = true;
-    subject:string = " ";
-    body:any = new EmptyQuill();
-    html:string = "";
+export class PromoterWelcomeEmailSettings implements IEmailSettings {
+    enabled = true;
+    subject = ' ';
+    body: any = new EmptyQuill();
+    html = '';
 }
 
-export class RegistrantWelcomeEmailSettings implements IEmailSettings{
-    enabled:boolean = true;
-    subject:string = " ";
-    body:any = new EmptyQuill();
-    html:string = "";
+export class RegistrantWelcomeEmailSettings implements IEmailSettings {
+    enabled = true;
+    subject = ' ';
+    body: any = new EmptyQuill();
+    html = '';
 }
 
-export class EventReminderEmailSettings implements IEmailSettings{
-    enabled:boolean = true;
-    daysBefore:number = 1;
-    subject:string = " ";
-    body:any = new EmptyQuill();
-    html:string = "";
+export class EventReminderEmailSettings implements IEmailSettings {
+    enabled = true;
+    daysBefore = 1;
+    subject = ' ';
+    body: any = new EmptyQuill();
+    html = '';
 }
 
-export class EventCompletionEmailSettings implements IEmailSettings{
-    enabled:boolean = true;
-    subject:string = " ";
-    body:any = new EmptyQuill();
-    daysAfter:number = 1;
-    html:string = "";
+export class EventCompletionEmailSettings implements IEmailSettings {
+    enabled = true;
+    subject = ' ';
+    body: any = new EmptyQuill();
+    daysAfter = 1;
+    html = '';
 }
 
-export class EventConfirmationSettings{
-    enabled:boolean = true;
-    message:any = new EmptyQuill();
+export class EventConfirmationSettings {
+    enabled = true;
+    message: any = new EmptyQuill();
 }
 
-export class EnabledRequired{
-    enabled:boolean;
-    required:boolean;
+export class EnabledRequired {
+    enabled: boolean;
+    required: boolean;
 
-    constructor(enabled, required){
+    constructor(enabled, required) {
         this.enabled = enabled;
         this.required = required;
     }
 
 }
 
-export class EmptyQuill{
+export class EmptyQuill {
 
-    ops:any = [ {
-        "insert" : " \n"
-    } ]
+    ops: any = [{
+        'insert': ' \n'
+    }];
 
 }
 
-export class EventFormFields{
+export class EventFormFields {
     $value;
     $exists;
-    prefix:EnabledRequired = new EnabledRequired(false, false);
-    firstName:EnabledRequired = new EnabledRequired(true, true);
-    lastName:EnabledRequired = new EnabledRequired(true, true);
-    suffix:EnabledRequired = new EnabledRequired(false, false);
-    email:EnabledRequired = new EnabledRequired(true, true);
-    phone:EnabledRequired = new EnabledRequired(true, true);
-    homeAddress:EnabledRequired = new EnabledRequired(false, false);
-    shippingAddress:EnabledRequired = new EnabledRequired(false, false);
-    website:EnabledRequired = new EnabledRequired(false, false);
-    gender:EnabledRequired = new EnabledRequired(false, false);
-    birthDate:EnabledRequired = new EnabledRequired(false, false);
-    age:EnabledRequired = new EnabledRequired(false, false);
-    doTerraAccount:EnabledRequired = new EnabledRequired(true, false);
-    doTerraRank:EnabledRequired = new EnabledRequired(false, false);
-    customFields:EnabledRequired[] = [];
+    prefix: EnabledRequired = new EnabledRequired(false, false);
+    firstName: EnabledRequired = new EnabledRequired(true, true);
+    lastName: EnabledRequired = new EnabledRequired(true, true);
+    suffix: EnabledRequired = new EnabledRequired(false, false);
+    email: EnabledRequired = new EnabledRequired(true, true);
+    phone: EnabledRequired = new EnabledRequired(true, true);
+    homeAddress: EnabledRequired = new EnabledRequired(false, false);
+    shippingAddress: EnabledRequired = new EnabledRequired(false, false);
+    website: EnabledRequired = new EnabledRequired(false, false);
+    gender: EnabledRequired = new EnabledRequired(false, false);
+    birthDate: EnabledRequired = new EnabledRequired(false, false);
+    age: EnabledRequired = new EnabledRequired(false, false);
+    doTerraAccount: EnabledRequired = new EnabledRequired(true, false);
+    doTerraRank: EnabledRequired = new EnabledRequired(false, false);
+    customFields: EnabledRequired[] = [];
 
-    //TYPES text (vanilla input), select, checkbox, multiselect
+    // TYPES text (vanilla input), select, checkbox, multiselect
     nameMap = {
-        prefix:{order:1, label:"Prefix", type:"text"},
-        firstName:{order:2, label:"First Name",type:"text"},
-        lastName:{order:3, label:"Last Name",type:"text"},
-        suffix:{order:4, label:"Suffix",type:"text"},
-        email:{order:5, label:"Email",type:'text'},
-        phone:{order:6, label:"Phone",type:'text'},
-        homeAddress:{order:7, label:"Home Address",type:'text'},
-        shippingAddress:{order:8, label:"Shipping Address",type:'text'},
-        website:{order:9, label:"Website",type:'text'},
-        gender:{order:10, label:"Gender",type:'select', values:[{value:"male", label:"Male"}, {value:"female", label:"Female"}]},
-        birthDate:{order:11, label:"Website",type:'date'},
-        age:{order:12, label:"Age",type:'text'},
-        doTerraAccount:{order:13, label:"Do you have a doTerra Wholesale account?",type:"checkbox"},
-        doTerraRank:{order:14, label:"What is your doTerra rank?",type:"select",values:[{value:"bronze", label:"Bronze"}, {value:"silver", label:"silver"}, {value:"gold", label:"gold"}]}
-    }
+        prefix: { order: 1, label: 'Prefix', type: 'text' },
+        firstName: { order: 2, label: 'First Name', type: 'text' },
+        lastName: { order: 3, label: 'Last Name', type: 'text' },
+        suffix: { order: 4, label: 'Suffix', type: 'text' },
+        email: { order: 5, label: 'Email', type: 'text' },
+        phone: { order: 6, label: 'Phone', type: 'text' },
+        homeAddress: { order: 7, label: 'Home Address', type: 'text' },
+        shippingAddress: { order: 8, label: 'Shipping Address', type: 'text' },
+        website: { order: 9, label: 'Website', type: 'text' },
+        gender: { order: 10, label: 'Gender', type: 'select', values:
+            [{ value: 'male', label: 'Male' }, { value: 'female', label: 'Female' }] },
+        birthDate: { order: 11, label: 'Website', type: 'date' },
+        age: { order: 12, label: 'Age', type: 'text' },
+        doTerraAccount: { order: 13, label: 'Do you have a doTerra Wholesale account?', type: 'checkbox' },
+        doTerraRank: { order: 14, label: 'What is your doTerra rank?', type: 'select', values:
+            [{ value: 'bronze', label: 'Bronze' }, { value: 'silver', label: 'silver' }, { value: 'gold', label: 'gold' }] }
+    };
 
 }
--- src/app/create/components/advanced/services/advanced.service.ts
+++ src/app/create/components/advanced/services/advanced.service.ts
@@ -1,52 +1,51 @@
-import {Injectable, OnDestroy, EventEmitter} from '@angular/core';
-import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
-import {EventSettings} from "../models/advanced-settings";
-import {AuthService} from "../../../../auth/services/auth-service";
+import { Injectable, OnDestroy, EventEmitter } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
+import { EventSettings } from '../models/advanced-settings';
+import { AuthService } from '../../../../auth/services/auth-service';
 
 @Injectable()
-export class CreateAdvancedService{
+export class CreateAdvancedService {
 
-    eventId:string;
-    eventSettings:EventSettings;
-    eventSettings$:FirebaseObjectObservable<any> = new FirebaseObjectObservable();
-    eventSet:boolean = false;
-    waitForSet:EventEmitter<any> = new EventEmitter();
+    eventId: string;
+    eventSettings: EventSettings;
+    eventSettings$: FirebaseObjectObservable<any> = new FirebaseObjectObservable();
+    eventSet = false;
+    waitForSet: EventEmitter<any> = new EventEmitter();
 
     constructor(private af: AngularFireDatabase,
-                private auth: AuthService
+        private auth: AuthService
     ) {
 
     }
 
-    setEventId(id){
+    setEventId(id) {
 
         console.log(id);
 
         this.eventId = id;
         this.eventSettings$ = this.af.object(`events/settings/${id}`);
 
-        //CHECK IF SETTINGS EXISTS, IF NOT CREATE AND SAVE NEW SETTINGS
-        this.checkExists().then((exists:boolean)=>{
-            if(!exists){
+        // CHECK IF SETTINGS EXISTS, IF NOT CREATE AND SAVE NEW SETTINGS
+        this.checkExists().then((exists: boolean) => {
+            if (!exists) {
                 this.eventSettings = new EventSettings();
-                this.eventSettings$.set(this.eventSettings).then(()=>{
+                this.eventSettings$.set(this.eventSettings).then(() => {
 
-                    //WATCH FOR ANY CHANGES TO THE SETTINGS AND UPDATE
-                    this.eventSettings$.subscribe((data)=>{
-                       this.eventSet = true;
+                    // WATCH FOR ANY CHANGES TO THE SETTINGS AND UPDATE
+                    this.eventSettings$.subscribe((data) => {
+                        this.eventSet = true;
 
-                       delete data.$exists;
-                       delete data.createdAt;
-                       delete data.$key;
+                        delete data.$exists;
+                        delete data.createdAt;
+                        delete data.$key;
 
-                       this.eventSettings = data;
+                        this.eventSettings = data;
                         this.waitForSet.emit();
                     });
-                })
-            }
-            else{
-                //WATCH FOR ANY CHANGES TO THE SETTINGS AND UPDATE
-                this.eventSettings$.subscribe((data)=>{
+                });
+            } else {
+                // WATCH FOR ANY CHANGES TO THE SETTINGS AND UPDATE
+                this.eventSettings$.subscribe((data) => {
                     this.eventSet = true;
 
                     delete data.$exists;
@@ -58,15 +57,15 @@ export class CreateAdvancedService{
                     this.waitForSet.emit();
                 });
             }
-        })
+        });
     }
 
-    checkExists():Promise<any>{
-        return new Promise((resolve, reject)=>{
-            this.eventSettings$.first().subscribe((data)=>{
-               resolve(data.$exists());
+    checkExists(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.eventSettings$.first().subscribe((data) => {
+                resolve(data.$exists());
             });
-        })
+        });
     }
 
 
