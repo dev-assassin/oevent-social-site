3921156184f95e8debbb927c2605032d05e895c2
ninjadev1030 <alexandra.uefa@gmail.com>
2018-07-03 00:39:52 +0800

    update follows.component.ts file

--- src/app/following/components/follows.component.ts
+++ src/app/following/components/follows.component.ts
@@ -1,52 +1,50 @@
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/pluck';
 
-import {Component, AfterContentInit, OnInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params} from '@angular/router';
-import {AuthService} from "../../auth/services/auth-service";
-import {AppService} from "../../services/app-service";
-import {FollowingService} from "../services/following.service";
+import { Component, AfterContentInit, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params } from '@angular/router';
+import { AuthService } from '../../auth/services/auth-service';
+import { AppService } from '../../services/app-service';
+import { FollowingService } from '../services/following.service';
 
 @Component({
     templateUrl: './follows.component.html',
     styles: [
-        `
-            
-        `
+        ``
     ],
 
 })
 
-export class FollowingComponent implements OnInit{
+export class FollowingComponent implements OnInit {
 
-    follows:string[];
+    follows: string[];
 
-    constructor(private auth: AuthService, private appService: AppService, private router: Router, private route: ActivatedRoute, private followingService: FollowingService) {
-
+    constructor(private auth: AuthService, private appService: AppService,
+        private router: Router, private route: ActivatedRoute, private followingService: FollowingService) {
     }
 
-    ngOnInit(){
+    ngOnInit() {
 
-        //POPULATE ONLY AFTER AUTH
-        if(this.appService.authChecked) {
+        // POPULATE ONLY AFTER AUTH
+        if (this.appService.authChecked) {
             this.populateFollows();
-        } else{
-            this.auth.authObservable.first().subscribe((data)=>{
+        } else {
+            this.auth.authObservable.first().subscribe((data) => {
                 this.populateFollows();
             });
         }
 
     }
 
-    populateFollows(){
-        this.followingService.getMyFollows().first().subscribe((data)=>{
+    populateFollows() {
+        this.followingService.getMyFollows().first().subscribe((data) => {
 
             this.follows = [];
 
-            for(let person of data){
+            for (const person of data) {
                 this.follows.push(person.$key);
             }
-        })
+        });
     }
 
 }
