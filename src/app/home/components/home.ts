2e1740f2f37dc1ed3e088ad379d5957930329f85
ninjadev1030 <alexandra.uefa@gmail.com>
2018-07-09 14:36:31 +0800

    update home.ts file

--- src/app/home/components/home.ts
+++ src/app/home/components/home.ts
@@ -1,18 +1,18 @@
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/pluck';
 
-import {Component, AfterContentInit, ViewChild, OnInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized} from '@angular/router';
-import {AuthService} from "../../auth/services/auth-service";
-import {SwiperConfigInterface, SwiperComponent} from "ngx-swiper-wrapper";
-import {AppService} from "../../services/app-service";
-import {SearchService} from "../../shared-module/services/search-service";
-import {ISearchQuery, SearchQuery} from "../../search/models/search-query";
-import {AngularFireDatabase} from "angularfire2/database/database";
-import {FirebaseListObservable} from "angularfire2/database/firebase_list_observable";
+import { Component, AfterContentInit, ViewChild, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized } from '@angular/router';
+import { AuthService } from '../../auth/services/auth-service';
+import { SwiperConfigInterface, SwiperComponent } from 'ngx-swiper-wrapper';
+import { AppService } from '../../services/app-service';
+import { SearchService } from '../../shared-module/services/search-service';
+import { ISearchQuery, SearchQuery } from '../../search/models/search-query';
+import { AngularFireDatabase } from 'angularfire2/database/database';
+import { FirebaseListObservable } from 'angularfire2/database/firebase_list_observable';
 
-declare var jQuery:any;
-declare var OtriggerSlides:any;
+declare var jQuery: any;
+declare var OtriggerSlides: any;
 
 @Component({
     templateUrl: './home.html',
@@ -24,37 +24,37 @@ declare var OtriggerSlides:any;
 export class HomeComponent implements AfterContentInit, OnInit {
 
     midNav = 1;
-    searchString = "";
-    location = "";
-    date = "all";
-    searchResults:any[] = [];
-    showNoResults:boolean = false;
-    defaultQuery:ISearchQuery = new SearchQuery();
-    featuredEvents$:FirebaseListObservable<any>;
-    featuredEvents:any[] = [];
-    getOcode:boolean = false;
+    searchString = '';
+    location = '';
+    date = 'all';
+    searchResults: any[] = [];
+    showNoResults = false;
+    defaultQuery: ISearchQuery = new SearchQuery();
+    featuredEvents$: FirebaseListObservable<any>;
+    featuredEvents: any[] = [];
+    getOcode = false;
 
     constructor(private auth: AuthService,
-                private router: Router,
-                private af: AngularFireDatabase,
-                private searchService:SearchService,
-                private appService:AppService) {
+        private router: Router,
+        private af: AngularFireDatabase,
+        private searchService: SearchService,
+        private appService: AppService) {
         jQuery('body').css('background-color', '#fff');
         router.events.subscribe((val) => {
-            if(val instanceof RoutesRecognized){
-                jQuery('body').css('background-color', '#f7f7f7')
+            if (val instanceof RoutesRecognized) {
+                jQuery('body').css('background-color', '#f7f7f7');
             }
         });
 
         OtriggerSlides();
 
-        this.featuredEvents$ = this.af.list(`/featured`)
+        this.featuredEvents$ = this.af.list(`/featured`);
 
     }
 
-    search(){
+    search() {
         let route = `/search?q=${this.searchString}&location=${this.location}&range=40mi`;
-        if(this.date != "all"){
+        if (this.date !== 'all') {
             route += `&date=${this.date}`;
         }
         this.router.navigateByUrl(route);
@@ -64,6 +64,7 @@ export class HomeComponent implements AfterContentInit, OnInit {
         this.router.navigate(['/account']);
     }
 
+    // tslint:disable-next-line:member-ordering
     public config: SwiperConfigInterface = {
         scrollbar: null,
         direction: 'horizontal',
@@ -80,40 +81,39 @@ export class HomeComponent implements AfterContentInit, OnInit {
         prevButton: '.swiper-button-prev'
     };
 
+    // tslint:disable-next-line:member-ordering
     @ViewChild(SwiperComponent) swiperView: SwiperComponent;
 
-    ngOnInit(){
+    ngOnInit() {
         this.runQuery();
-        this.featuredEvents$.subscribe((data)=>{
+        this.featuredEvents$.subscribe((data) => {
             this.featuredEvents = data;
-        })
+        });
     }
 
-    parseResults(data){
+    parseResults(data) {
 
         console.log(data.$exists());
 
-        if(data.$exists()){
+        if (data.$exists()) {
             console.log(data);
 
-            let cleanData:any;
+            let cleanData: any;
 
 
 
-            //TODO DROP THIS IF WHEN EVERYTHING IS THE SAME (RIGHT NOW THE NODE ONE AND C# ONE RETURN DIFFERENT TYPES)
-            if(typeof data.$value == "string"){
+            // TODO DROP THIS IF WHEN EVERYTHING IS THE SAME (RIGHT NOW THE NODE ONE AND C# ONE RETURN DIFFERENT TYPES)
+            if (typeof data.$value === 'string') {
                 cleanData = (JSON.parse(data.$value));
-            }
-            else{
+            } else {
                 cleanData = data;
             }
 
 
-            if(cleanData.data && cleanData.data.total > 0){
+            if (cleanData.data && cleanData.data.total > 0) {
                 this.showNoResults = false;
                 this.searchResults = cleanData.data.hits;
-            }
-            else {
+            } else {
                 this.searchResults = [];
                 this.showNoResults = true;
             }
@@ -123,48 +123,48 @@ export class HomeComponent implements AfterContentInit, OnInit {
         this.appService.stopLoadingBar();
     }
 
-    runQuery(){
+    runQuery() {
 
-        let $this = this;
+        const $this = this;
 
-        this.searchService.getCurrentLocation().then((location)=>{
+        this.searchService.getCurrentLocation().then((location) => {
 
             this.defaultQuery.locationSet = true;
             this.defaultQuery.location = location;
             console.log(this.defaultQuery);
 
-            this.searchService.buildQuery(this.defaultQuery, 15, 0, true).then(function(data){
+            this.searchService.buildQuery(this.defaultQuery, 15, 0, true).then(function (data) {
 
                 $this.appService.stopLoadingBar();
-                //send over key of passed results
+                // send over key of passed results
                 $this.getResults();
 
-            }, function(err){
+            }, function (err) {
                 $this.appService.stopLoadingBar();
                 console.log(err);
-            })
-        })
+            });
+        });
 
     }
 
-    getResults():void{
-        this.searchService.updateResults().then((data$)=>{
-            data$.subscribe((data)=>{
-                //console.log(data);
+    getResults(): void {
+        this.searchService.updateResults().then((data$) => {
+            data$.subscribe((data) => {
+                // console.log(data);
                 this.parseResults(data);
             });
-        }, (err)=>{
+        }, (err) => {
             console.log(err);
         });
     }
 
-    ngAfterContentInit(){
-        jQuery(document).ready(function($){
-            var slidesWrapper = $('.cd-hero-slider');
+    ngAfterContentInit() {
+        jQuery(document).ready(function ($) {
+            const slidesWrapper = $('.cd-hero-slider');
 
-            //check if a .cd-hero-slider exists in the DOM
-            if ( slidesWrapper.length > 0 ) {
-                var primaryNav = $('.cd-primary-nav'),
+            // check if a .cd-hero-slider exists in the DOM
+            if (slidesWrapper.length > 0) {
+                const primaryNav = $('.cd-primary-nav'),
                     sliderNav = $('.cd-hero .prev, .cd-hero .next'),
                     navigationMarker = $('.cd-marker'),
                     slidesNumber = slidesWrapper.children('li').length,
@@ -172,54 +172,51 @@ export class HomeComponent implements AfterContentInit, OnInit {
                     autoPlayId,
                     autoPlayDelay = 7000;
 
-                //upload videos (if not on mobile devices)
+                // upload videos (if not on mobile devices)
                 uploadVideo(slidesWrapper);
 
-                //autoplay slider
+                // autoplay slider
                 setAutoplay(slidesWrapper, slidesNumber, autoPlayDelay);
 
-                //on mobile - open/close primary navigation clicking/tapping the menu icon
-                primaryNav.on('click', function(event){
-                    if($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
+                // on mobile - open/close primary navigation clicking/tapping the menu icon
+                primaryNav.on('click', function (event) {
+                    // tslint:disable-next-line:curly
+                    if ($(event.target).is('.cd-primary-nav')) $(this).children('ul').toggleClass('is-visible');
                 });
 
-                //change visible slide - Shane Loveland customized to wrk with left and right arrows
-                sliderNav.on('click', function(event){
+                // change visible slide - Shane Loveland customized to wrk with left and right arrows
+                sliderNav.on('click', function (event) {
                     event.preventDefault();
-                    var selectedItem = $(this);
+                    const selectedItem = $(this);
 
-                    var prev = selectedItem.hasClass("prev");
-                    var prevTarget, nextTarget, setPrev;
-                    var length = slidesWrapper.find('>li').length;
-                    var current = slidesWrapper.find('.selected').index() + 1;
+                    const prev = selectedItem.hasClass('prev');
+                    const prevTarget, nextTarget, setPrev;
+                    const length = slidesWrapper.find('>li').length;
+                    const current = slidesWrapper.find('.selected').index() + 1;
 
-                    if(prev){
-                        if(current === 1){
-                            prevTarget = length-1;
+                    if (prev) {
+                        if (current === 1) {
+                            this.prevTarget = length - 1;
+                        } else {
+                            this.prevTarget = current - 2;
                         }
-                        else{
-                            prevTarget = current-2;
-                        }
-                    }
-                    else{
-                        if(current === length){
-                            prevTarget = 0;
-                            setPrev = true;
-                        }
-                        else{
-                            nextTarget = current;
+                    } else {
+                        if (current === length) {
+                            this.prevTarget = 0;
+                            this.setPrev = true;
+                        } else {
+                            this.nextTarget = current;
                         }
                     }
 
-                    if(selectedItem.hasClass("prev") || setPrev){
+                    if (selectedItem.hasClass('prev') || setPrev) {
                         prevSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, prevTarget);
                         setAutoplay(slidesWrapper, slidesNumber, autoPlayDelay);
-                        setPrev = false;
-                    }
-                    else{
+                        this.setPrev = false;
+                    } else {
                         nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, nextTarget);
                         setAutoplay(slidesWrapper, slidesNumber, autoPlayDelay);
-                        setPrev = false;
+                        this.setPrev = false;
                     }
 
                 });
@@ -227,8 +224,9 @@ export class HomeComponent implements AfterContentInit, OnInit {
 
             }
 
-            function nextSlide(visibleSlide, container, pagination, n){
-                visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+            function nextSlide(visibleSlide, container, pagination, n) {
+                visibleSlide.removeClass('selected from-left from-right').addClass('is-moving')
+                    .one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
                     visibleSlide.removeClass('is-moving');
                 });
 
@@ -236,8 +234,9 @@ export class HomeComponent implements AfterContentInit, OnInit {
                 checkVideo(visibleSlide, container, n);
             }
 
-            function prevSlide(visibleSlide, container, pagination, n){
-                visibleSlide.removeClass('selected from-left from-right').addClass('is-moving').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+            function prevSlide(visibleSlide, container, pagination, n) {
+                visibleSlide.removeClass('selected from-left from-right').addClass('is-moving')
+                    .one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
                     visibleSlide.removeClass('is-moving');
                 });
 
@@ -246,69 +245,74 @@ export class HomeComponent implements AfterContentInit, OnInit {
             }
 
             function updateSliderNavigation(pagination, n) {
-                var navigationDot = pagination.find('.selected');
+                const navigationDot = pagination.find('.selected');
                 navigationDot.removeClass('selected');
                 pagination.find('li').eq(n).addClass('selected');
             }
 
             function setAutoplay(wrapper, length, delay) {
-                if(wrapper.hasClass('autoplay')) {
-                    clearInterval(autoPlayId);
-                    autoPlayId = window.setInterval(function(){autoplaySlider(length)}, delay);
+                if (wrapper.hasClass('autoplay')) {
+                    const autoplay;
+                    clearInterval(this.autoPlayId);
+                    this.autoPlayId = window.setInterval(function () { autoplaySlider(length); }, delay);
                 }
             }
 
             function autoplaySlider(length) {
-                if( visibleSlidePosition < length - 1) {
+                const visibleSlidePosition, sliderNav;
+                if (visibleSlidePosition < length - 1) {
                     nextSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, visibleSlidePosition + 1);
-                    visibleSlidePosition +=1;
+                    this.visibleSlidePosition += 1;
                 } else {
                     prevSlide(slidesWrapper.find('.selected'), slidesWrapper, sliderNav, 0);
-                    visibleSlidePosition = 0;
+                    this.visibleSlidePosition = 0;
                 }
-                updateNavigationMarker(navigationMarker, visibleSlidePosition+1);
+                updateNavigationMarker(this.navigationMarker, visibleSlidePosition + 1);
                 updateSliderNavigation(sliderNav, visibleSlidePosition);
             }
 
             function uploadVideo(container) {
-                container.find('.cd-bg-video-wrapper').each(function(){
-                    var videoWrapper = $(this);
-                    if( videoWrapper.is(':visible') ) {
+                container.find('.cd-bg-video-wrapper').each(function () {
+                    const videoWrapper = $(this);
+                    if (videoWrapper.is(':visible')) {
                         // if visible - we are not on a mobile device
-                        var	videoUrl = videoWrapper.data('video'),
-                            video = $('<video loop><source src="'+videoUrl+'.mp4" type="video/mp4" /><source src="'+videoUrl+'.webm" type="video/webm" /></video>');
+                        const videoUrl = videoWrapper.data('video'),
+                            video = $('<video loop><source src='' + videoUrl + '.mp4' type='video/mp4' /><source src='' + videoUrl + '.webm' type='video/webm' /></video>');
                         video.appendTo(videoWrapper);
                         // play video if first slide
-                        if(videoWrapper.parent('.cd-bg-video.selected').length > 0) video.get(0).play();
+                        // tslint:disable-next-line:curly
+                        if (videoWrapper.parent('.cd-bg-video.selected').length > 0) video.get(0).play();
                     }
                 });
             }
 
             function checkVideo(hiddenSlide, container, n) {
-                //check if a video outside the viewport is playing - if yes, pause it
-                var hiddenVideo = hiddenSlide.find('video');
-                if( hiddenVideo.length > 0 ) hiddenVideo.get(0).pause();
+                // check if a video outside the viewport is playing - if yes, pause it
+                const hiddenVideo = hiddenSlide.find('video');
+                // tslint:disable-next-line:curly
+                if (hiddenVideo.length > 0) hiddenVideo.get(0).pause();
 
-                //check if the select slide contains a video element - if yes, play the video
-                var visibleVideo = container.children('li').eq(n).find('video');
-                if( visibleVideo.length > 0 ) visibleVideo.get(0).play();
+                // check if the select slide contains a video element - if yes, play the video
+                const visibleVideo = container.children('li').eq(n).find('video');
+                // tslint:disable-next-line:curly
+                if (visibleVideo.length > 0) visibleVideo.get(0).play();
             }
 
             function updateNavigationMarker(marker, n) {
-                marker.removeClassPrefix('item').addClass('item-'+n);
+                marker.removeClassPrefix('item').addClass('item-' + n);
             }
 
-            $.fn.removeClassPrefix = function(prefix) {
-                //remove all classes starting with 'prefix'
-                this.each(function(i, el) {
-                    var classes = el.className.split(" ").filter(function(c) {
+            $.fn.removeClassPrefix = function (prefix) {
+                // remove all classes starting with 'prefix'
+                this.each(function (i, el) {
+                    const classes = el.className.split(' ').filter(function (c) {
                         return c.lastIndexOf(prefix, 0) !== 0;
                     });
-                    el.className = $.trim(classes.join(" "));
+                    el.className = $.trim(classes.join(' '));
                 });
                 return this;
             };
-        });/**
+        }); /**
          * Created by sloveland on 6/16/16.
          */
 
