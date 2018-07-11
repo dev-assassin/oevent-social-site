d05adb930597a39b2c300dd404725fa1e67336ba
ninjadev1030 <alexandra.uefa@gmail.com>
2018-07-12 07:05:07 +0800

    update landing.ts file

--- src/app/landing/components/landing.ts
+++ src/app/landing/components/landing.ts
@@ -1,13 +1,13 @@
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/pluck';
 
-import {Component, AfterContentInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized} from '@angular/router';
-import {AuthService} from "../../auth/services/auth-service";
-import {AppService} from "../../services/app-service";
+import { Component, AfterContentInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized } from '@angular/router';
+import { AuthService } from '../../auth/services/auth-service';
+import { AppService } from '../../services/app-service';
 
-declare var jQuery:any;
-declare var OtriggerSlides:any;
+declare var jQuery: any;
+declare var OtriggerSlides: any;
 
 
 @Component({
@@ -20,54 +20,39 @@ declare var OtriggerSlides:any;
 export class LandingComponent {
 
     constructor(private auth: AuthService,
-                private router: Router,
-                public appService: AppService) {
+        private router: Router,
+        public appService: AppService) {
         jQuery('body').css('background-color', '#fff');
 
         this.hideHeader();
 
-        if(this.auth.authenticated){
+        if (this.auth.authenticated) {
             this.showHeader();
-        }
-        else{
-            this.auth.authObservable.subscribe(()=>{
-                if(this.auth.authenticated){
+        } else {
+            this.auth.authObservable.subscribe(() => {
+                if (this.auth.authenticated) {
                     this.showHeader();
-                }
-                else{
+                } else {
                     this.hideHeader();
                 }
             });
         }
 
         router.events.subscribe((val) => {
-            if(val instanceof RoutesRecognized){
-                jQuery('body').css('background-color', '#f7f7f7')
+            if (val instanceof RoutesRecognized) {
+                jQuery('body').css('background-color', '#f7f7f7');
             }
         });
     }
 
-    hideHeader(){
+    hideHeader() {
         jQuery('app-header, footer.c-layout-footer').css('display', 'none');
         jQuery('html').css('marginTop', '0');
     }
 
-    showHeader(){
+    showHeader() {
         jQuery('app-header, footer.c-layout-footer').css('display', 'block');
         jQuery('html').css('marginTop', '90px');
     }
-
-
-
-    /** COMING BACK LATER **
-    search(){
-        let route = `/search?q=${this.searchString}&location=${this.location}&range=40mi`;
-        if(this.date != "all"){
-            route += `&date=${this.date}`;
-        }
-        this.router.navigateByUrl(route);
-    }*/
-
-
 
 }
