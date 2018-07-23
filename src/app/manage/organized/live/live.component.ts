55fdb5f813025521743066c07949cb0777dfde6c
ninjadev1030 <alexandra.uefa@gmail.com>
2018-07-23 17:34:29 +0800

    update live component

--- src/app/manage/organized/live/live.component.ts
+++ src/app/manage/organized/live/live.component.ts
@@ -1,13 +1,13 @@
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/pluck';
 
-import {Component, AfterContentInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized} from '@angular/router';
-import {AuthService} from "../../../auth/services/auth-service";
-import {ManageService} from "../../../shared-module/services/manage-service";
-import {AngularFireDatabase} from "angularfire2/database"
-import {IoEvent} from "../../../shared-models/oevent";
-import {FirebaseListObservable} from "angularfire2/database/firebase_list_observable";
+import { Component, AfterContentInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth-service';
+import { ManageService } from '../../../shared-module/services/manage-service';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { IoEvent } from '../../../shared-models/oevent';
+import { FirebaseListObservable } from 'angularfire2/database/firebase_list_observable';
 
 @Component({
     templateUrl: './live.html',
@@ -18,22 +18,22 @@ import {FirebaseListObservable} from "angularfire2/database/firebase_list_observ
             border-color: rgba(135,151,174,.15);
             padding:15px 0;
            }
-           
+
            @media(min-width: 776px){
                 .c-cart-sub-title{
                     display:none;
-                }     
+                }
             }
     `
     ]
 })
 
-export class OrganizedLiveComponent{
+export class OrganizedLiveComponent {
 
-    events:IoEvent[] = [];
-    activeEvents$:FirebaseListObservable<any[]>;
+    events: IoEvent[] = [];
+    activeEvents$: FirebaseListObservable<any[]>;
 
-    constructor(private auth: AuthService, private router: Router, public manageService: ManageService, private af:AngularFireDatabase) {
+    constructor(private auth: AuthService, private router: Router, public manageService: ManageService, private af: AngularFireDatabase) {
         this.activeEvents$ = this.manageService.getEvents('active');
     }
 
