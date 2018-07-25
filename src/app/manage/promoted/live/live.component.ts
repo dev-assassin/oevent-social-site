d2b9b1a1b76d9da779319874e8e6eaa376cd240c
ninjadev1030 <alexandra.uefa@gmail.com>
2018-07-25 10:09:52 +0800

    add live.component.ts file

---
+++ src/app/manage/promoted/live/live.component.ts
@@ -0,0 +1,33 @@
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/pluck';
+
+import {Component, AfterContentInit} from '@angular/core';
+import {Router, NavigationEnd, RoutesRecognized} from '@angular/router';
+import {AuthService} from "../../../auth/services/auth-service";
+import {ManagePromotionsService} from "../../../shared-module/services/manage-promotions.service";
+
+@Component({
+    template: `
+        <div class="row"> 
+            <div 
+            manage-card 
+            class="col-sm-6" 
+            *ngFor="let event of promotionsService.myPromotions$ | async" 
+            [eventId]="event.$key" 
+            [eventType]="'promotedLive'"></div>
+        </div>
+    `,
+    styles: [
+
+    ]
+})
+
+export class PromotedLiveComponent{
+
+    constructor(private auth: AuthService,
+                private router: Router,
+                public promotionsService: ManagePromotionsService) {
+
+    }
+
+}
