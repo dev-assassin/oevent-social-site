5e0e2575f5aa854af72480f5a8dfed5701b43f4d
ninjadev1030 <alexandra.uefa@gmail.com>
2018-07-26 15:47:55 +0800

    update event-details.component.ts

--- src/app/manage/promoted/manage-promoted/event-details.component.ts
+++ src/app/manage/promoted/manage-promoted/event-details.component.ts
@@ -1,25 +1,25 @@
-import {Component, AfterContentInit, OnInit, Input, AfterViewInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params} from '@angular/router';
-import {AuthService} from "../../../auth/services/auth-service";
-import {IoEvent} from "../../../shared-models/oevent";
-import {NgbActiveModal, NgbModal} from "@ng-bootstrap/ng-bootstrap";
-import {EventService} from "../../../event/services/event-service";
-declare var Quill:any;
+import { Component, AfterContentInit, OnInit, Input, AfterViewInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth-service';
+import { IoEvent } from '../../../shared-models/oevent';
+import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { EventService } from '../../../event/services/event-service';
+declare var Quill: any;
 
 @Component({
-    selector: 'my-event-details',
+    selector: 'app-my-event-details',
     template: `
         <div *ngIf="eventService.event">
             <h3 class="line" style="margin-bottom:0px;">
-                {{ eventService.event.title }}                
+                {{ eventService.event.title }}
             </h3>
-            
+
             <p>
-                <a routerLink="/event/{{ eventService.eventId }}" target="_blank"> 
+                <a routerLink="/event/{{ eventService.eventId }}" target="_blank">
                     <i class="fa fa-eye"></i> <span style="font-size: 15px"> View landing page</span>
                 </a>
             </p>
-            
+
             <div class="row row-vertical" style="padding-top:2rem;">
                 <div class="col-md-3">
                     <p><strong>Status:</strong></p>
@@ -28,176 +28,175 @@ declare var Quill:any;
                     <p>
                         <span [ngClass]="eventService.event.liveStatus">
                             {{ eventService.event.liveStatus }}
-                        </span>                                                              
+                        </span>
                     </p>
                 </div>
             </div>
-            
+
             <div class="row row-vertical">
                 <div class="col-md-3">
                     <p><strong>Schedule:</strong></p>
                 </div>
                 <div class="col-md-9">
                     <p>
-                        {{ eventService.event.date }} | {{ eventService.event.time }}                  
+                        {{ eventService.event.date }} | {{ eventService.event.time }}
                     </p>
                 </div>
             </div>
-            
+
             <div class="row row-vertical" *ngIf="eventService.event.type == 'local'">
                 <div class="col-md-3">
                     <p><strong>Location:</strong></p>
                 </div>
                 <div class="col-md-9">
                     <p>
-                        {{ eventService.event.location }}                    
+                        {{ eventService.event.location }}
                     </p>
                 </div>
             </div>
-            
+
             <div class="row row-vertical" *ngIf="eventService.event.type == 'online'">
                 <div class="col-md-3">
                     <p><strong>URL:</strong></p>
                 </div>
                 <div class="col-md-9">
                     <p>
-                        {{ eventService.event.location }}                    
+                        {{ eventService.event.location }}
                     </p>
                 </div>
             </div>
-            
+
             <div class="row row-vertical">
                 <div class="col-md-3">
                     <p><strong>Description:</strong></p>
                 </div>
                 <div class="col-md-9" style="padding-top:7px;padding-bottom:15px;">
-                    
+
                     <div class="c-desc" >
                         <div id="toolbar"></div>
                         <div id="description" style="" ></div>
-                    </div>                    
-                    
+                    </div>
+
                 </div>
             </div>
-            
+
             <div class="row row-vertical">
                 <div class="col-md-3">
                     <p><strong>Category:</strong></p>
                 </div>
                 <div class="col-md-9">
                     <p>
-                        {{ eventService.event.category }}                    
+                        {{ eventService.event.category }}
                     </p>
                 </div>
             </div>
-            
+
             <div class="row row-vertical">
                 <div class="col-md-3">
                     <p><strong>Visibility:</strong></p>
                 </div>
                 <div class="col-md-9">
                     <p>
-                        {{ eventService.event.visibility }}                    
+                        {{ eventService.event.visibility }}
                     </p>
                 </div>
             </div>
-            
+
             <div class="row row-vertical">
                 <div class="col-md-3">
                     <p><strong>Keywords:</strong></p>
                 </div>
                 <div class="col-md-9">
                     <p>
-                        {{ keywordString }}                        
+                        {{ keywordString }}
                     </p>
                 </div>
             </div>
-            
+
             <div class="row row-vertical" style="padding-top:1rem;">
                 <div class="col-md-3">
                     <p><strong>Registration Types:</strong></p>
                 </div>
                 <div class="col-md-9">
-                    <table class="register-table"> 
-                        <tbody> 
-                            <tr *ngFor="let ticket of eventService.eventTickets$ | async"> 
-                                <td> 
+                    <table class="register-table">
+                        <tbody>
+                            <tr *ngFor="let ticket of eventService.eventTickets$ | async">
+                                <td>
                                     {{ ticket.ticketTitle }}
                                 </td>
-                                <td> 
+                                <td>
                                     {{ ticket.ticketType }}
                                 </td>
-                                <td> 
+                                <td>
                                     {{ ticket.ticketsUsed }}/{{ ticket.ticketQuantity }}
-                                </td>    
+                                </td>
                             </tr>
                         </tbody>
-                    </table>                    
+                    </table>
                 </div>
             </div>
-            
+
         </div>
-        
+
     `,
     styles: [
         `
             .register-table tr td{
-                padding-right:2rem;    
+                padding-right:2rem;
             }
-            
+
             .register-table tr td:last-child{
                 padding-right:0;
-            } 
-            
+            }
+
             .row.row-vertical{
                 padding-bottom:1rem;
             }
-            
-            
+
         `
     ],
     providers: [NgbActiveModal]
 
 })
 
-export class PromotedEventDetailsComponent implements OnInit, AfterViewInit{
+export class PromotedEventDetailsComponent implements OnInit, AfterViewInit {
 
-    @Input() event:IoEvent;
-    description:any;
-    keywordString:string = "";
-    keywordsBuilt:boolean = false;
+    @Input() event: IoEvent;
+    description: any;
+    keywordString = '';
+    keywordsBuilt = false;
 
-    //TRACK IF DATA HAS BEEN LOADED FROM FIREBASE
-    set:boolean = false;
+    // TRACK IF DATA HAS BEEN LOADED FROM FIREBASE
+    set = false;
 
     constructor(private auth: AuthService,
-                private router: Router,
-                private route: ActivatedRoute,
-                public eventService: EventService,
-                private activeModal:NgbActiveModal,
-                private modalService:NgbModal) {
+        private router: Router,
+        private route: ActivatedRoute,
+        public eventService: EventService,
+        private activeModal: NgbActiveModal,
+        private modalService: NgbModal) {
 
     }
 
-    ngOnInit(){
+    ngOnInit() {
 
     }
 
-    ngAfterViewInit(){
+    ngAfterViewInit() {
 
-        //USING SET INTERVAL UNTIL THE DATA IS SET SINCE THE PROPER CONTAINER IS NOT LOADED UNTIL SET IT SET
-        let doCheck = setInterval(()=>{
+        // USING SET INTERVAL UNTIL THE DATA IS SET SINCE THE PROPER CONTAINER IS NOT LOADED UNTIL SET IT SET
+        const doCheck = setInterval(() => {
 
-            if(this.eventService.set){
+            if (this.eventService.set) {
 
-                if(!this.keywordsBuilt){
+                if (!this.keywordsBuilt) {
                     this.buildKeywordString();
                 }
 
-                //ALLOW TIME FOR ANG2 TO REACT
-                setTimeout(()=>{
+                // ALLOW TIME FOR ANG2 TO REACT
+                setTimeout(() => {
 
-                    //INSTANTIATE BASED ON ID
+                    // INSTANTIATE BASED ON ID
                     this.description = new Quill('#description', {
                         readOnly: true
                     });
@@ -212,20 +211,20 @@ export class PromotedEventDetailsComponent implements OnInit, AfterViewInit{
         }, 100);
     }
 
-    openPromoter(){
+    openPromoter() {
 
     }
 
-    buildKeywordString(){
+    buildKeywordString() {
 
-        let keywords:string[] = this.eventService.event.keywords;
+        const keywords: string[] = this.eventService.event.keywords;
         console.log(keywords);
-        let keywordString:string = "";
+        let keywordString = '';
 
         for (let i = 0; i < keywords.length; i++) {
             keywordString += keywords[i];
-            if((i+1)<keywords.length){
-                keywordString +=", ";
+            if ((i + 1) < keywords.length) {
+                keywordString += ', ';
             }
         }
 
