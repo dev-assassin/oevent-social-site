7ddefa571406383d93046469309362fc8c6c4254
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-06 14:52:06 +0800

    update manage.component.ts file

--- src/app/manage/promoted/manage-promoted/manage.component.ts
+++ src/app/manage/promoted/manage-promoted/manage.component.ts
@@ -1,14 +1,14 @@
-import {Component, AfterContentInit, OnInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized, Params, ActivatedRoute} from '@angular/router';
-import {AuthService} from "../../../auth/services/auth-service";
-import {ManagePromotionsService} from "../../../shared-module/services/manage-promotions.service";
-import {EventService} from "../../../event/services/event-service";
+import { Component, AfterContentInit, OnInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized, Params, ActivatedRoute } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth-service';
+import { ManagePromotionsService } from '../../../shared-module/services/manage-promotions.service';
+import { EventService } from '../../../event/services/event-service';
 
 @Component({
     template: `
 
-        <section class="secondary-content"> 
-            <div class="container"> 
+        <section class="secondary-content">
+            <div class="container">
                 <ul class="breadcrumbs">
                     <li>
                         <a routerLink="/manage">
@@ -16,62 +16,57 @@ import {EventService} from "../../../event/services/event-service";
                         </a>
                     </li>
                     <li>
-                        <a routerLink="/manage/promoted"> 
+                        <a routerLink="/manage/promoted">
                             Promoted Events
                         </a>
                     </li>
-                    <li *ngIf="eventService.set"> 
+                    <li *ngIf="eventService.set">
                         {{ eventService.event.title }}
                     </li>
                 </ul>
-                
-                <div class="row"> 
-                    <div class="col-md-3"> 
+
+                <div class="row">
+                    <div class="col-md-3">
                         <manage-promoted-menu></manage-promoted-menu>
                     </div>
-                    
-                    <div class="col-md-9" *ngIf="eventService.eventId"> 
+
+                    <div class="col-md-9" *ngIf="eventService.eventId">
                         <router-outlet></router-outlet>
                     </div>
-                </div>        
-                
+                </div>
             </div>
         </section>
-
-
-        
     `,
     styles: [
 
     ]
 })
 
-export class ManagePromotedComponent implements OnInit{
+export class ManagePromotedComponent implements OnInit {
 
-    eventId:string;
+    eventId: string;
 
     constructor(private auth: AuthService,
-                private router: Router,
-                private route: ActivatedRoute,
-                public eventService: EventService,
-                public promotionsService: ManagePromotionsService) {
+        private router: Router,
+        private route: ActivatedRoute,
+        public eventService: EventService,
+        public promotionsService: ManagePromotionsService) {
 
     }
 
-    ngOnInit(): void{
+    ngOnInit(): void {
 
         this.route.params.forEach((params: Params) => {
 
+            // tslint:disable-next-line:no-shadowed-variable
             this.route.params.forEach((params: Params) => {
 
-                if(!params['id']){
-                    this.eventService.inviteKey = "promoter";
-                }
-
-                else{
+                if (!params['id']) {
+                    this.eventService.inviteKey = 'promoter';
+                } else {
                     this.eventId = params['id'];
-                    this.eventService.setEvent(params['id']).then(()=>{
-                        this.eventService.inviteKey = "promoter";
+                    this.eventService.setEvent(params['id']).then(() => {
+                        this.eventService.inviteKey = 'promoter';
                     });
                 }
             });
