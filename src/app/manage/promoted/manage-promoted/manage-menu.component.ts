33eadfa449b36a90876cdd2a6062d7592a0f6bf1
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-06 10:21:32 +0800

    update manage-menu.component.ts file

--- src/app/manage/promoted/manage-promoted/manage-menu.component.ts
+++ src/app/manage/promoted/manage-promoted/manage-menu.component.ts
@@ -1,69 +1,65 @@
-import {Component, AfterContentInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized} from '@angular/router';
-import {AuthService} from "../../../auth/services/auth-service";
-import {ManagePromotionsService} from "../../../shared-module/services/manage-promotions.service";
+import { Component, AfterContentInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth-service';
+import { ManagePromotionsService } from '../../../shared-module/services/manage-promotions.service';
 
 @Component({
-    selector: 'manage-promoted-menu',
+    selector: 'app-manage-promoted-menu',
     template: `
-        <div class="sec-nav"> 
-            <div class="c-layout-sidebar-menu c-theme"> 
-                <div class="c-sidebar-menu-toggler"> 
+        <div class="sec-nav">
+            <div class="c-layout-sidebar-menu c-theme">
+                <div class="c-sidebar-menu-toggler">
                     <h3 class="c-title c-font-uppercase c-font-bold">Manage Promotion</h3>
                 </div>
-                
+
                 <a class="c-content-toggler" data-target="#sidebar-menu-1" data-toggle="collapse" href="javascript:;">
                     <span _ngcontent-c4="" class="c-line"></span>
                     <span _ngcontent-c4="" class="c-line"></span>
                     <span _ngcontent-c4="" class="c-line"></span>
                 </a>
-                
-                <ul class="c-sidebar-menu collapse" id="sidebar-menu-1"> 
-                    <li class="c-dropdown c-open"> 
+
+                <ul class="c-sidebar-menu collapse" id="sidebar-menu-1">
+                    <li class="c-dropdown c-open">
                         <a class="c-toggler">Management</a>
-                        <ul class="c-dropdown-menu"> 
-                            <li routerLinkActive="active"> 
-                                <a routerLink="event-details"> 
+                        <ul class="c-dropdown-menu">
+                            <li routerLinkActive="active">
+                                <a routerLink="event-details">
                                     Event Details
                                 </a>
                             </li>
-                            <li routerLinkActive="active"> 
-                                <a routerLink="my-registrations"> 
+                            <li routerLinkActive="active">
+                                <a routerLink="my-registrations">
                                     My registrations
                                 </a>
                             </li>
-                            <li routerLinkActive="active"> 
-                                <a routerLink="registration-types"> 
+                            <li routerLinkActive="active">
+                                <a routerLink="registration-types">
                                     Registration types
                                 </a>
                             </li>
-                            
-                            
                         </ul>
                     </li>
-                    <li class="c-dropdown c-open"> 
+                    <li class="c-dropdown c-open">
                         <a class="c-toggler">Communication</a>
-                        <ul class="c-dropdown-menu"> 
-                            <li routerLinkActive="active"> 
-                                <a routerLink="invite"> 
+                        <ul class="c-dropdown-menu">
+                            <li routerLinkActive="active">
+                                <a routerLink="invite">
                                     Invite to attend
                                 </a>
                             </li>
-                            <li routerLinkActive="active"> 
-                                <a routerLink="send-email"> 
+                            <li routerLinkActive="active">
+                                <a routerLink="send-email">
                                     Send email
                                 </a>
                             </li>
-                           <!-- <li routerLinkActive="active"> 
-                                <a routerLink="sent-messages"> 
+                           <!-- <li routerLinkActive="active">
+                                <a routerLink="sent-messages">
                                     Sent messages
                                 </a>
                             </li> -->
                         </ul>
                     </li>
-                    
                 </ul>
-                
             </div>
         </div>
     `,
@@ -72,11 +68,11 @@ import {ManagePromotionsService} from "../../../shared-module/services/manage-pr
     ]
 })
 
-export class PromotedManageMenuComponent{
+export class PromotedManageMenuComponent {
 
     constructor(private auth: AuthService,
-                private router: Router,
-                public promotionsService: ManagePromotionsService) {
+        private router: Router,
+        public promotionsService: ManagePromotionsService) {
 
     }
 
