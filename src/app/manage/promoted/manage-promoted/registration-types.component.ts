cf9cd90cea5ca38a5ea7959f98090d9f4ae3b2a8
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-06 21:17:41 +0800

    update registration-types.component.ts file

--- src/app/manage/promoted/manage-promoted/registration-types.component.ts
+++ src/app/manage/promoted/manage-promoted/registration-types.component.ts
@@ -1,41 +1,36 @@
-import {Component, OnInit} from '@angular/core';
-import {AngularFireDatabase} from "angularfire2/database"
-import {EventService} from "../../../event/services/event-service";
+import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { EventService } from '../../../event/services/event-service';
 
 @Component({
-    selector:'my-event-registration-type',
+    selector: 'app-my-event-registration-type',
     template: `
         <h3 class="line" style="margin-bottom:0px;">
             {{ eventService.event.title }} - Registration Types
         </h3>
-        
+
         <div class="row row-eq-height">
-            <div 
-                *ngFor="let regType of eventService.eventTickets$ | async" 
-                promoter-registration-type-detail 
-                [regType]="regType" 
+            <div
+                *ngFor="let regType of eventService.eventTickets$ | async"
+                promoter-registration-type-detail
+                [regType]="regType"
                 class="col-md-4">
                 </div>
-                
-                
         </div>
-        
     `,
     styles: [
-        `
-       
-        `
+        ``
     ]
 
 })
 
-export class PromotedRegistrationTypesComponent implements OnInit{
+export class PromotedRegistrationTypesComponent implements OnInit {
 
     constructor(public eventService: EventService) {
 
     }
 
-    ngOnInit(){
+    ngOnInit() {
 
     }
 }
