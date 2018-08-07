6f15344de39b44692f8630b81c49dc01b0561bf4
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-07 16:22:23 +0800

    add promoted.component.ts file

---
+++ src/app/manage/promoted/promoted.component.ts
@@ -0,0 +1,34 @@
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/pluck';
+
+import {Component, AfterContentInit} from '@angular/core';
+import {Router, NavigationEnd, RoutesRecognized} from '@angular/router';
+import {AuthService} from "../../auth/services/auth-service";
+import {ManagePromotionsService} from "../../shared-module/services/manage-promotions.service";
+
+@Component({
+    templateUrl: './promoted.html',
+    styles: [
+        `
+            .nav-tabs .nav-item {
+                margin-bottom: -1px;
+                min-width: 120px;
+                text-align: center;
+            }
+            
+            .light-bottom-border{
+                border-bottom: 1px solid #eaeaea;
+            }
+        `
+    ]
+})
+
+export class PromotedComponent{
+
+    constructor(private auth: AuthService,
+                private router: Router,
+                public promotionsService: ManagePromotionsService) {
+
+    }
+
+}
