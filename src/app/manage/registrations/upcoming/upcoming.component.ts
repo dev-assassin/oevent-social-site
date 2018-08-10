1f3973e8e06c3e316eb1f8d260d06b55cd418d95
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-10 20:20:38 +0800

    update upcoming.component.ts file

--- src/app/manage/registrations/upcoming/upcoming.component.ts
+++ src/app/manage/registrations/upcoming/upcoming.component.ts
@@ -1,19 +1,19 @@
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/pluck';
 
-import {Component, AfterContentInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized} from '@angular/router';
-import {AuthService} from "../../../auth/services/auth-service";
-import {ManageRegistrationsService} from "../manage-registrations.service";
+import { Component, AfterContentInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth-service';
+import { ManageRegistrationsService } from '../manage-registrations.service';
 
 @Component({
     template: `
         <div class="row">
-            <div 
-            manage-card 
-            class="col-sm-6" 
-            *ngFor="let event of promotionsService.myRegistrations$ | async" 
-            [eventId]="event.$key" 
+            <div
+            manage-card
+            class="col-sm-6"
+            *ngFor="let event of promotionsService.myRegistrations$ | async"
+            [eventId]="event.$key"
             [eventType]="'attendingLive'"></div>
         </div>
     `,
@@ -22,11 +22,11 @@ import {ManageRegistrationsService} from "../manage-registrations.service";
     ]
 })
 
-export class RegistrationsUpcomingComponent{
+export class RegistrationsUpcomingComponent {
 
     constructor(private auth: AuthService,
-                private router: Router,
-                public promotionsService: ManageRegistrationsService) {
+        private router: Router,
+        public promotionsService: ManageRegistrationsService) {
 
     }
 
