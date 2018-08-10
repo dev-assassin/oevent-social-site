de166f5f6303bb08c8c398b96e0a332827c2b665
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-10 11:08:32 +0800

    update history.component.ts file

--- src/app/manage/registrations/history/history.component.ts
+++ src/app/manage/registrations/history/history.component.ts
@@ -1,10 +1,10 @@
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/pluck';
 
-import {Component, AfterContentInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized} from '@angular/router';
-import {AuthService} from "../../../auth/services/auth-service";
-import {ManageRegistrationsService} from "../manage-registrations.service";
+import { Component, AfterContentInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth-service';
+import { ManageRegistrationsService } from '../manage-registrations.service';
 
 @Component({
     templateUrl: './history.html',
@@ -13,11 +13,11 @@ import {ManageRegistrationsService} from "../manage-registrations.service";
     ]
 })
 
-export class RegistrationsHistoryComponent{
+export class RegistrationsHistoryComponent {
 
     constructor(private auth: AuthService,
-                private router: Router,
-                public promotionsService: ManageRegistrationsService) {
+        private router: Router,
+        public promotionsService: ManageRegistrationsService) {
 
     }
 
