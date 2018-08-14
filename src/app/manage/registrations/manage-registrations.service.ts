7f4257ee6c82f3180efcd6fab9daa77d6862e836
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-14 16:45:14 +0800

    update manage-registrations.service.ts file

--- src/app/manage/registrations/manage-registrations.service.ts
+++ src/app/manage/registrations/manage-registrations.service.ts
@@ -1,34 +1,29 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import {AppService} from "../../services/app-service";
-import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database"
-import {AuthService} from "../../auth/services/auth-service";
+import { AppService } from '../../services/app-service';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { AuthService } from '../../auth/services/auth-service';
 
 @Injectable()
 export class ManageRegistrationsService {
 
-    myPromotions$:FirebaseListObservable<any>;
-    myRegistrations$:FirebaseListObservable<any>;
+    myPromotions$: AngularFireList<any>;
+    myRegistrations$: AngularFireList<any>;
 
-    constructor(private af:AngularFireDatabase, private auth:AuthService){
-        this.myPromotions$ = this.af.list(`/promoters/users/${this.auth.id}`, {
-            //TODO FIGURE OUT HOW AND WHEN TO FILTER OUT OLD EVENTS
-        });
+    constructor(private af: AngularFireDatabase, private auth: AuthService) {
+        this.myPromotions$ = this.af.list(`/promoters/users/${this.auth.id}`);
 
-        this.myRegistrations$ = this.af.list(`/registrations/users/${this.auth.id}`, {
-            //TODO FILTER OUT OLD ETC...
-        });
+        this.myRegistrations$ = this.af.list(`/registrations/users/${this.auth.id}`);
 
 
     }
 
-    eventRegistration(id):Promise<FirebaseListObservable<any>>{
-        return new Promise((resolve, reject)=>{
-            if(!this.auth.authenticated){
-                this.auth.authObservable.first().subscribe(()=>{
+    eventRegistration(id): Promise<AngularFireList<any>> {
+        return new Promise((resolve, reject) => {
+            if (!this.auth.authenticated) {
+                this.auth.authObservable.first().subscribe(() => {
                     resolve(this.af.list(`/registrations/users/${this.auth.id}/${id}`));
-                })
-            }
-            else{
+                });
+            } else {
                 resolve(this.af.list(`/registrations/users/${this.auth.id}/${id}`));
             }
         });
