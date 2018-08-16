897cf7a5063b7a72016ccc97ea18c7421f22a61e
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-16 16:56:13 +0800

    update registrations components

--- src/app/manage/registrations/registrations.component.ts
+++ src/app/manage/registrations/registrations.component.ts
@@ -1,10 +1,10 @@
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/pluck';
 
-import {Component, AfterContentInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized} from '@angular/router';
-import {AuthService} from "../../auth/services/auth-service";
-import {ManageRegistrationsService} from "./manage-registrations.service";
+import { Component, AfterContentInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized } from '@angular/router';
+import { AuthService } from '../../auth/services/auth-service';
+import { ManageRegistrationsService } from './manage-registrations.service';
 
 @Component({
     templateUrl: './promoted.html',
@@ -15,7 +15,7 @@ import {ManageRegistrationsService} from "./manage-registrations.service";
                 min-width: 120px;
                 text-align: center;
             }
-            
+
             .light-bottom-border{
                 border-bottom: 1px solid #eaeaea;
             }
@@ -23,7 +23,7 @@ import {ManageRegistrationsService} from "./manage-registrations.service";
     ]
 })
 
-export class RegistrationsComponent{
+export class RegistrationsComponent {
 
     constructor(private auth: AuthService, private router: Router, public promotionsService: ManageRegistrationsService) {
 
