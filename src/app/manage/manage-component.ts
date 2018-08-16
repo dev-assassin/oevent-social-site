8486738cf92450e72c7f6edfa3eaf3d8668ca5be
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-16 18:24:31 +0800

    add manage-component.ts file

---
+++ src/app/manage/manage-component.ts
@@ -0,0 +1,33 @@
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/pluck';
+
+import {Component, AfterContentInit} from '@angular/core';
+import {Router, NavigationEnd, RoutesRecognized} from '@angular/router';
+import {AuthService} from "../auth/services/auth-service";
+
+@Component({
+    templateUrl: './manage.html',
+    styleUrls: [
+        './manage.scss'
+    ]
+})
+
+export class ManageComponent{
+
+    constructor(private auth: AuthService, private router: Router) {
+
+    }
+
+    gotoOrganized(){
+        this.router.navigateByUrl('manage/organized/live');
+    }
+
+    gotoPromoted(){
+        this.router.navigateByUrl('manage/promoted/live');
+    }
+
+    gotoTickets(){
+        this.router.navigateByUrl('manage/attending/upcoming');
+    }
+
+}
