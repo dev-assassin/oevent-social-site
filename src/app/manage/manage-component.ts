3d11dda5d0a2ff0be0b11c00374449aca01c09ff
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-16 19:25:35 +0800

    update manage-component.ts file

--- src/app/manage/manage-component.ts
+++ src/app/manage/manage-component.ts
@@ -1,9 +1,9 @@
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/pluck';
 
-import {Component, AfterContentInit} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized} from '@angular/router';
-import {AuthService} from "../auth/services/auth-service";
+import { Component, AfterContentInit } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized } from '@angular/router';
+import { AuthService } from '../auth/services/auth-service';
 
 @Component({
     templateUrl: './manage.html',
@@ -12,21 +12,21 @@ import {AuthService} from "../auth/services/auth-service";
     ]
 })
 
-export class ManageComponent{
+export class ManageComponent {
 
     constructor(private auth: AuthService, private router: Router) {
 
     }
 
-    gotoOrganized(){
+    gotoOrganized() {
         this.router.navigateByUrl('manage/organized/live');
     }
 
-    gotoPromoted(){
+    gotoPromoted() {
         this.router.navigateByUrl('manage/promoted/live');
     }
 
-    gotoTickets(){
+    gotoTickets() {
         this.router.navigateByUrl('manage/attending/upcoming');
     }
 
