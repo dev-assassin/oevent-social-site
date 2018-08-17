287fc779785f5f5c7906a6c7143d7602f4fbfc4e
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-17 22:51:18 +0800

    add membership-options.component.ts file

---
+++ src/app/marketing/components/membership-options.component.ts
@@ -0,0 +1,59 @@
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/pluck';
+
+import {Component, OnInit} from '@angular/core';
+import {AppService} from "../../services/app-service";
+import {ToastyService} from "ng2-toasty";
+import {AuthService} from "../../auth/services/auth-service";
+import {MembershipService} from "../services/membership.service";
+
+@Component({
+    templateUrl: './membership-options.component.html',
+    styles: [
+        `
+            .curr-level{margin-top:13px;}
+        `
+    ]
+})
+
+export class MembershipOptionsComponent implements OnInit{
+
+    set:boolean = false;
+    membership:string;
+
+    constructor(private appService: AppService, private toasty: ToastyService, private auth: AuthService, private membershipService: MembershipService) {
+        this.appService.startLoadingBar();
+    }
+
+    ngOnInit(){
+        this.membershipService.getMembership().then((data)=>{
+
+
+
+            if(data.$exists()){
+                this.membership = data.membershipType;
+                this.set = true;
+                this.appService.stopLoadingBar();
+            }
+            else{
+
+                this.membershipService.setMembership("free").then(()=>{
+                    this.membership = "free";
+                    this.appService.stopLoadingBar();
+                    this.set = true;
+                });
+            }
+
+        })
+    }
+
+    updateMembership(membership:string){
+        this.appService.startLoadingBar();
+        this.membershipService.setMembership(membership).then(()=>{
+            this.appService.stopLoadingBar();
+            this.membership = membership;
+            this.toasty.success(`Membership updated to ${membership}`);
+        });
+    }
+
+}
