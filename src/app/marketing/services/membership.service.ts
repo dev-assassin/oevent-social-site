525d3c655425b836b4d6393013473c0c40f8d9ca
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-20 08:45:58 +0800

    update membership.service.ts file

--- src/app/marketing/services/membership.service.ts
+++ src/app/marketing/services/membership.service.ts
@@ -2,32 +2,32 @@ import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/switchMap';
 
 import { Injectable } from '@angular/core';
-import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
-import {IoEvent} from "../../shared-models/oevent";
-import {Observable} from "rxjs";
-import {AuthService} from "../../auth/services/auth-service";
+import { AngularFireDatabase, AngularFireList, AngularFireObject } from 'angularfire2/database';
+import { IoEvent } from '../../shared-models/oevent';
+import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../../auth/services/auth-service';
 
 @Injectable()
 export class MembershipService {
 
-    membership$:FirebaseObjectObservable<any> = new FirebaseObjectObservable();
+    membership$: AngularFireObject<any>;
 
     constructor(public af: AngularFireDatabase, public auth: AuthService) {
         this.membership$ = this.af.object(`/membership/${this.auth.id}`);
     }
 
-    getMembership():Promise<any>{
+    getMembership(): Promise<any> {
 
-        return new Promise((resolve, reject)=>{
-            this.membership$.first().subscribe((data)=>{
+        return new Promise((resolve, reject) => {
+            this.membership$.first().subscribe((data) => {
                 resolve(data);
             });
         });
 
     }
 
-    setMembership(membershipType:string){
-        return this.membership$.set({membershipType:membershipType});
+    setMembership(membershipType: string) {
+        return this.membership$.set({ membershipType: membershipType });
     }
 
 }
