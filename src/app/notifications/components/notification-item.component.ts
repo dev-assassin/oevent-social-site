0933a9b3c8268c56cbab5acfed9903a1fad0b8ec
ninjadev1030 <alexandra.uefa@gmail.com>
2018-08-21 22:28:32 +0800

    update notification-item component ts file

--- src/app/notifications/components/notification-item.component.ts
+++ src/app/notifications/components/notification-item.component.ts
@@ -1,37 +1,37 @@
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/pluck';
 
-import {Component, AfterContentInit, OnInit, Input} from '@angular/core';
-import {Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params} from '@angular/router';
-import {FirebaseOperation} from "angularfire2/database/firebase_list_observable";
-import {Message} from "../models/Message";
-import {AuthService} from "../../auth/services/auth-service";
-import {InappNotificationService} from "../../shared-module/services/inapp-notification.service";
+import { Component, AfterContentInit, OnInit, Input } from '@angular/core';
+import { Router, NavigationEnd, RoutesRecognized, ActivatedRoute, Params } from '@angular/router';
+import { FirebaseOperation } from 'angularfire2';
+import { Message } from '../models/Message';
+import { AuthService } from '../../auth/services/auth-service';
+import { InappNotificationService } from '../../shared-module/services/inapp-notification.service';
 
 
 @Component({
-    selector:'notif-item',
+    selector: 'app-notif-item',
     templateUrl: './notification-item.component.html',
     styles: [
-        `           
+        `
         `
     ],
 })
 
-export class NotificationItemComponent implements OnInit{
+export class NotificationItemComponent implements OnInit {
     @Input() message: Message;
-    @Input() mini:boolean;
+    @Input() mini: boolean;
 
-    constructor(private auth: AuthService,private inAppNotificationService:InappNotificationService) {
-
-    }
-
-    ngOnInit(){
+    constructor(private auth: AuthService, private inAppNotificationService: InappNotificationService) {
 
     }
 
-    public archiveMessage(key:FirebaseOperation){
-        this.inAppNotificationService.removeMessage(key,this.auth.id);
+    ngOnInit() {
+
+    }
+
+    public archiveMessage(key: FirebaseOperation) {
+        this.inAppNotificationService.removeMessage(key, this.auth.id);
     }
 
 }
